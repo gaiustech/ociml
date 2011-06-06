e7018dcd0a9caafb2ccb69a8922070007f5387ae
Gaius <github@gaius.org.uk>
2011-06-06 22:56:47 +0100

    added oci_bulkdml.c

---
+++ oci_bulkdml.c
@@ -0,0 +1,18 @@
+/* functions relating to array DML */
+
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+#include <caml/alloc.h>
+#include <caml/custom.h>
+#include <caml/callback.h>
+#include <caml/fail.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <oci.h>
+#include <ocidfn.h>
+#include "oci_wrapper.h"
+
+
+
+/* end of file */
